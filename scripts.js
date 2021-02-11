70098764d03da55dcefa65013a26cb8aca94f889
Patrick Almeida Silva <palmeids@everis.com>
2021-02-10 23:45:09 -0300

    Salvando os dados no localStorage

--- scripts.js
+++ scripts.js
@@ -11,24 +11,19 @@ const Modal = {
   }
 }
 
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
+  },
+
+  set(transactions) {
+    localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+  }
+}
+
 const Transaction = {
-  all: [
-    {
-      description: 'Luz',
-      amount: -50000,
-      date: '23/01/2021'
-    },
-    {
-      description: 'Website',
-      amount: 50000,
-      date: '23/01/2021'
-    },
-    {
-      description: 'Internet',
-      amount: -20000,
-      date: '23/01/2021'
-    },
-  ],
+  all: Storage.get(),
+
   add(transaction){
     Transaction.all.push(transaction);
     App.reload();
@@ -198,6 +193,7 @@ const App = {
     });
     
     DOM.updateBalance();
+    Storage.set(Transaction.all)
   },
   reload() {
     DOM.clearTransactions();
